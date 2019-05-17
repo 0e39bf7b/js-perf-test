635e0a6fcca379d5e220de50fe5e8c010b6829ad
0e39bf7b <0e39bf7b@gmail.com>
2019-05-17 18:02:05 +0300

    Add test images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import lenna from './lenna.jpg';
+import empty from './empty.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={lenna} className="App-logo" alt="Lenna" />
+        <img src={empty} className="App-logo" alt="Empty" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
